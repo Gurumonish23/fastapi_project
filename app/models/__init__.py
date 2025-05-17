8455bd2c7b69afddf0a7cb73809211a9db658902
Gurumonish23 <76999092+Gurumonish23@users.noreply.github.com>
2025-05-17 06:47:51 +0530

    Create app/models/__init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+# Import all models to make them available for use in the application
+from .example_model import ExampleModel
+
+# This file serves as a package initializer for the models module
+# It allows for easy import of all models from this module
+# Example usage:
+# from app.models import ExampleModel
