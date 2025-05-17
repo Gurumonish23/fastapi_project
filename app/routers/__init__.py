de9ec45e8ca0868d6a2367774b456a38ea7fa960
Gurumonish23 <76999092+Gurumonish23@users.noreply.github.com>
2025-05-17 06:47:54 +0530

    Create app/routers/__init__.py

---
+++ app/routers/__init__.py
@@ -0,0 +1,7 @@
+# Import all routers to make them available for use in the application
+from .example import router as example_router
+
+# This file serves as a package initializer for the routers module
+# It allows for easy import of all routers from this module
+# Example usage:
+# from app.routers import example_router
