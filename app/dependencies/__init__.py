f32e36122a526b2338bca2e1cec65f197522dd48
Gurumonish23 <76999092+Gurumonish23@users.noreply.github.com>
2025-05-17 06:47:48 +0530

    Create app/dependencies/__init__.py

---
+++ app/dependencies/__init__.py
@@ -0,0 +1,7 @@
+# Import all dependencies to make them available for use in the application
+from .example_dependency import get_query_token, verify_user
+
+# This file serves as a package initializer for the dependencies module
+# It allows for easy import of all dependencies from this module
+# Example usage:
+# from app.dependencies import get_query_token, verify_user
