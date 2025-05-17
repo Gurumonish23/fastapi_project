83935c0c757666e964e094bffb651c4b637ace26
Gurumonish23 <76999092+Gurumonish23@users.noreply.github.com>
2025-05-17 06:47:57 +0530

    Create app/schemas/__init__.py

---
+++ app/schemas/__init__.py
@@ -0,0 +1,7 @@
+# Import all schemas to make them available for use in the application
+from .example_schema import ExampleSchema, ExampleCreateSchema
+
+# This file serves as a package initializer for the schemas module
+# It allows for easy import of all schemas from this module
+# Example usage:
+# from app.schemas import ExampleSchema, ExampleCreateSchema
